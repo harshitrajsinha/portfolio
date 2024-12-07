5db0e0b454e9733744162cbebbd6462dfe2cfa5a
Harshit <raj.harshitsinha08@gmail.com>
2024-12-07 13:53:47 +0530

    intro font size change

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".intro").textContent = "Harshit Raj Sinha";
         if (frameWidth < frameHeight) {
           document.querySelector(".intro").style.fontSize =
-            String(newIntroFont / 1.5) + "rem";
+            String(newIntroFont / 1.2) + "rem";
           document.querySelector(".skill-desc").style.fontSize =
             String(newIntroFont / 1.8) + "rem";
           document.querySelector(".prev-experience").style.fontSize =
