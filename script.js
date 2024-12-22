c2e3ce641fec159b3f2068f932603a5430beac14
Harshit <raj.harshitsinha08@gmail.com>
2024-12-22 19:32:43 +0530

    refactoring

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("script loaded");
+
   const fixedPhotoHeight = 518.4;
   const fixedPhotoWidth = 407.35;
   const fixedIntroFont = 5;
@@ -17,57 +19,29 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".intro")
   ).fontSize;
 
-  // ResizeObserver to notify change in window
-  const observer = new ResizeObserver((entries) => {
-    for (let entry of entries) {
-      const frameWidth = window.innerWidth;
-      const frameHeight = window.innerHeight;
+  window.addEventListener("resize", function (e) {
+    const frameWidth = window.innerWidth;
+    const frameHeight = window.innerHeight;
 
-      const newPhotoHeight =
-        (frameHeight / fixedFrameHeight) * fixedPhotoHeight;
-      const newPhotoWidth = (frameWidth / fixedFrameWidth) * fixedPhotoWidth;
-      const newIntroFont = (frameWidth / fixedFrameWidth) * fixedIntroFont + 1;
-      if (
-        frameWidth === Math.floor(entry.contentRect.width) &&
-        frameWidth <= 1024
-      ) {
-        document.querySelector(".exclamation").style.display = "none";
-        document.querySelector(".intro").textContent = "Harshit Raj Sinha";
-        if (frameWidth < frameHeight) {
-          document.querySelector(".intro").style.fontSize =
-            String(newIntroFont / 1.2) + "rem";
-          document.querySelector(".skill-desc").style.fontSize =
-            String(newIntroFont / 1.8) + "rem";
-          document.querySelector(".prev-experience").style.fontSize =
-            String(newIntroFont / 2.5) + "rem";
-          if (frameWidth <= 540) {
-            document.querySelector(".my-photo").style.width =
-              String(newPhotoWidth + 60) + "px";
-          } else {
-            document.querySelector(".my-photo").style.width =
-              String(newPhotoWidth + 0) + "px";
-          }
-          document.querySelector(".my-photo").style.height =
-            String(newPhotoHeight) + "px";
-        } else {
-          document.querySelector(".intro").style.fontSize =
-            String(newIntroFont) + "rem";
-          document.querySelector(".skill-desc").style.fontSize =
-            String(newIntroFont / 2) + "rem";
-          document.querySelector(".prev-experience").style.fontSize =
-            String(newIntroFont / 3) + "rem";
-          document.querySelector(".my-photo").style.width = prevPhotoWidth;
-          document.querySelector(".my-photo").style.height = prevPhotoHeight;
-        }
-      } else if (
-        frameWidth === Math.floor(entry.contentRect.width) &&
-        frameWidth > 1024
-      ) {
-        document.querySelector(".exclamation").style.display = "block";
-        document.querySelector(".intro").textContent = "I'm Harshit";
+    const newPhotoHeight = (frameHeight / fixedFrameHeight) * fixedPhotoHeight;
+    const newPhotoWidth = (frameWidth / fixedFrameWidth) * fixedPhotoWidth;
+    const newIntroFont = (frameWidth / fixedFrameWidth) * fixedIntroFont + 1;
+    if (frameWidth <= 1024) {
+      if (frameWidth < frameHeight) {
+        document.querySelector("#intro-ss").style.fontSize =
+          String(newIntroFont / 1.2) + "rem";
+        document.querySelector("#skill-desc").style.fontSize =
+          String(newIntroFont / 1.8) + "rem";
+        document.querySelector("#prev-experience").style.fontSize =
+          String(newIntroFont / 2.5) + "rem";
+      } else {
+        document.querySelector("#intro-ss").style.fontSize =
+          String(newIntroFont) + "rem";
+        document.querySelector("#skill-desc").style.fontSize =
+          String(newIntroFont / 2) + "rem";
+        document.querySelector("#prev-experience").style.fontSize =
+          String(newIntroFont / 3) + "rem";
       }
     }
   });
-
-  observer.observe(document.body);
 });
