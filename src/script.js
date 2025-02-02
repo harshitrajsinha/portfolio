27bb7d41700b4f3964d46819172ab6942e6d6e98
Harshit <raj.harshitsinha08@gmail.com>
2025-02-02 23:02:08 +0530

    adding video resume

--- src/script.js
+++ src/script.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM is loaded");
+  const openBtn = document.getElementById("openVideo");
+  const modal = document.getElementById("videoModal");
+  const closeBtn = document.getElementById("closeVideo");
+  const video = document.getElementById("videoPlayer");
+
+  // Close Modal on Click Outside Video
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+      video.pause();
+    }
+  });
+
+  // Open Modal & Play Video
+  openBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+    video.currentTime = 0;
+    video.play();
+  });
+
+  // Close Modal & Pause Video
+  closeBtn.addEventListener("click", () => {
+    video.pause();
+    modal.style.display = "none";
+  });
 });
